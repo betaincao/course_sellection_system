3aae548250ee2c04563c63440547f25929ed905b
caoyouming <943844391@qq.com>
2018-05-02 18:27:17 +0800

    新增课程教师管理

--- application/manage/controller/Appoint.php
+++ application/manage/controller/Appoint.php
@@ -6,15 +6,29 @@ header("Content-Type: text/html; charset=utf-8");
      * 指定代课教师
      */
 class Appoint extends Base{
+
     public function index(){
+        $department = \think\Db::name('teacher')->field('t_department')->order('t_num')->group('t_department')->select();
+        foreach($department as $value){
+            $info[$value['t_department']] = \think\Db::name('teacher')->field('name')->where('t_department',$value['t_department'])->select();
+        }
+        //print_r($info);die;
+        $this->assign('info',$info);
+        return $this->fetch();
+    }
+
+
+
+    public function appoint(){
         if(request()->isPost()){
             $c_id = input('c_id');
-            $t_num = input('t_num');
+            $department = input('province');
+            $name = input('city');
+            $teacherinfo = \think\Db::name('teacher')->field('t_num')->where('t_department',$department)->where('name',$name)->find();
             $data = [
                 'id'=>$c_id,
-                't_num'=>$t_num,
+                't_num'=>$teacherinfo['t_num'],
             ];
-            if(\think\Db::table('system_teacher')->where('t_num',$t_num)->find()){
                 $res = \think\Db::table('system_ct')->insert($data,$replace=true);
                 $this->success('成功','choose/index');
                 // $course = \think\Db::table('system_course')->field('c_name')->where('c_id',$c_id)->find();
@@ -22,9 +36,6 @@ class Appoint extends Base{
                 // $this->assign('course',$course);
                 // $this->assign('course',$course);
                 // return $this->fetch('ctlist');
-            }else{
-                $this->error('您输入的教工号不正确，请返回重新输入');
-            }
         }
         return $this->fetch(); 
     }
--- application/manage/controller/Courseteacher.php
+++ application/manage/controller/Courseteacher.php
@@ -9,15 +9,23 @@ class Courseteacher extends Base{
 
     public function index(){
         $course = \think\Db::name('ct')->select();
-        $c_id = array();
-        $t_num = array();
-        foreach($course as $key=>$value){
-            $c_id[] = $value['id'];
-            $t_num[] = $value['t_num'];
+        if($course){
+            $c_id = array();
+            $t_num = array();
+            foreach($course as $key=>$value){
+                // $data[]['course'] = \think\Db::name('course')->field('c_grade,c_major,c_num,c_name')->where('c_id',$value['id'])->find();
+                // $data[]['teacher'] = \think\Db::name('teacher')->field('t_num,name')->where('t_num',$value['t_num'])->find();
+                $data[]=[
+                    'course' => \think\Db::name('course')->field('c_grade,c_major,c_num,c_name')->where('c_id',$value['id'])->find(),
+                    'teacher' => \think\Db::name('teacher')->field('t_num,name')->where('t_num',$value['t_num'])->find(),
+                ];
+            }
+            //print_r($data);die;
+            $this->assign('data',$data);
+            return $this->fetch();
+        }else{
+            $this->error('暂无数据');
         }
-        $courseName = \think\Db::name('course')->field('c_grade,c_major,c_num,c_name')->where('c_id','in',$c_id)->select();
-        $teacherName = \think\Db::name('teacher')->field('t_num,name')->where('t_num','in',$t_num)->select();
-        var_dump($courseName);
-        //var_dump($teacherName);die;
     }
+        
 }
